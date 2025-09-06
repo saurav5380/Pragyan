ef0b95816a715d67f3aab468fbf40441ab70580c
saurav5380 <saurav5380@gmail.com>
2025-09-06 23:41:56 +0530

    universe build v1

--- services/api/app/routes/universe.py
+++ services/api/app/routes/universe.py
@@ -2,13 +2,16 @@ import os
 from kiteconnect import KiteConnect
 import pandas as pd
 import pandas_ta as ta
+import numpy as np
+from datetime import time
 
 
 api_key = os.environ.get("KITE_API_KEY")
 access_key = os.environ.get("KITE_ACCESS_TOKEN")
 
 def get_all_stocks():
-    instruments = KiteConnect.instruments(exchange="NSE")
+    kite = KiteConnect(api_key=api_key)
+    instruments = kite.instruments(exchange="NSE")
     equities = [
         {
             "exchange": inst["exchange"],
@@ -24,5 +27,34 @@ def get_all_stocks():
     ]
     return equities
 
+def avg_daily_volume(df, period=20):
+    df[f"ADV{period}"] = ta.sma(df['v'], length=period)
+    return df
+
+def avg_true_range(df, period=14):
+    df[f"ATR{period}"] = ta.atr(df['h'], df['l'], df['c'], length=period)
+    return df
+
+def fetch_all_stocks():
+    kite = KiteConnect(api_key=api_key)
+    all_quotes = {}
+    try:
+        equity = get_all_stocks()
+        batch_size = 500
+
+        for i in range(0,len(equity), batch_size):
+            batch = equity[i : i+batch_size]
+            quotes = kite.quote(batch)
+            all_quotes.update(quotes)
+
+    except Exception as e:
+        print(f"Falied to retrieve data: {e}")
+    
+    time.sleep(5)
+
+    return all_quotes
+
+
+        
 
 
