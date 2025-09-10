d0a8843672f502c2bfa4d813c529ed0df55645a9
saurav5380 <saurav5380@gmail.com>
2025-09-10 10:39:06 +0530

    Universe Creation update-1

--- services/api/app/routes/universe.py
+++ services/api/app/routes/universe.py
@@ -9,24 +9,6 @@ from datetime import time
 api_key = os.environ.get("KITE_API_KEY")
 access_key = os.environ.get("KITE_ACCESS_TOKEN")
 
-def get_all_stocks():
-    kite = KiteConnect(api_key=api_key)
-    instruments = kite.instruments(exchange="NSE")
-    equities = [
-        {
-            "exchange": inst["exchange"],
-            "ticker": inst["tradingsymbol"],
-            "name": inst["name"],
-            "sector": inst.get("sector") or "",
-            "tick_size": float(inst.get("tick_size", 0.05)),
-            "instrument_token": inst.get("instrument_token", True),
-            "last_price": inst.get("last_price", 0.0)
-        }
-        for inst in instruments
-        if inst.get("segment") == "NSE" and inst.get("instrument_type") == "EQ"
-    ]
-    return equities
-
 def avg_daily_volume(df, period=20):
     df[f"ADV{period}"] = ta.sma(df['v'], length=period)
     return df
@@ -35,11 +17,12 @@ def avg_true_range(df, period=14):
     df[f"ATR{period}"] = ta.atr(df['h'], df['l'], df['c'], length=period)
     return df
 
-def fetch_all_stocks():
+def fetch_stock_quotes():
     kite = KiteConnect(api_key=api_key)
     all_quotes = {}
+    all_instruments = kite.instruments(exchange="NSE")
     try:
-        equity = get_all_stocks()
+        equity = [inst["tradingsymbol"] for inst in all_instruments if inst["instrument_type"] == "EQ"]
         batch_size = 500
 
         for i in range(0,len(equity), batch_size):
@@ -53,6 +36,11 @@ def fetch_all_stocks():
     time.sleep(5)
 
     return all_quotes
+
+get_all_stock_quotes = fetch_stock_quotes()
+
+
+
 
 
         
